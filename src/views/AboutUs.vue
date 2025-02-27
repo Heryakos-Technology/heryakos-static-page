40480e76b5ff82aa370b951af3e23f585613c898
complatform <kalkidansol1623@gmail.com>
2025-02-28 00:22:48 +0300

    Add the about us page to the view

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,205 @@
+<template>
+    
+    <Navbar/>
+    <div class="mt-12 z-0">
+<div>
+
+    <img src="/team_photo1.png" alt="" class="h-68 relative lg:w-full lg:h-8/12">
+</div>
+<div class="absolute top-36 left-5 lg:left-20 lg:top-74  text-white">
+
+    <h1 class="lg:text-4xl lg:font-bold">FINANCIAL FOCUS <br>
+        & CONSULTING </h1>
+        <h2 class="mt-3 lg:mt-8 lg:text-lg lg:font-semibold">With over years of experiance <br class="lg:hidden">
+ business to fine <br class="">
+comprehensive solutions </h2>
+<div class="lg:mt-12 mt-6">
+
+    <a href="#" class="bg-white text-purple-800 px-5 py-2 lg:px-7 lg:py-3 mt-4 rounded-lg lg:rounded-xl">LEARN MORE</a>
+    <a href="#" class="button text-white px-5 py-2 lg:px-7 lg:py-3 mt-4 rounded-lg lg:rounded-xl ml-4">CONTACT US</a>
+</div>
+</div>
+    </div>
+    <div class="middleContainer mt-20 rounded-md shadow-sm flex lg:justify-between ml-3 mr-3 lg:ml-12 lg:mr-12">
+        <div class="pl-4 pt-6 pb-5 lg:mt-5 lg:ml-10 ">
+            <h1 class=" font-semibold lg:text-3xl ">Welcome to Financial Company</h1>
+            <h2 class="mt-5 text-sm w-60 lg:w-2/3 lg:text-lg">We are a passionate team 
+dedicated to providing 
+high-quality content and 
+resources that inspire and inform 
+our audience. With a focus on 
+innovation andcreativity, we strive 
+to deliver engaging experiences 
+that resonate with our visitors 
+Our mission is to connect,
+educate, and empower individuals 
+through our carefully curated 
+articles and insights..</h2>
+<div class="mt-6 lg:mt-5">
+
+    <a href="#" class="button text-white px-8 py-2  rounded-xl">KNOW MORE</a>
+</div>
+        </div>
+        <div class="w-full lg:w-full"> 
+            
+            <img src="/teamsmall2.png" alt="" class="h-full  rounded-md  lg:hidden">
+            <img src="/teallarge2.jpg" alt="" class="h-full w-full rounded-md hidden lg:block lg:visible lg:h-full lg:w-full lg:rounded-xl">
+        </div>
+    </div>
+    <div class="lastContainer mt-20 rounded-sm shadow-sm pb-5 ml-3 mr-3 lg:ml-12 lg:mr-12 ">
+<h1 class="text-center font-semibold pt-5 lg:text-3xl">Our Teams</h1>
+<div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10">
+    <div class="photoContainer w-56 rounded-md shadow-md shadow-gray-400 relative group lg:w-1/2 lg:h-96 h-60 ">
+        
+        <div>
+            <img src="/businessmanlarge1.jpg" alt="" class="w-full rounded-md h-44 lg:h-80">
+            <div class="hoverContainer absolute inset-0 rounded-md opacity-0 group-hover:opacity-75 flex items-center justify-center transition-opacity h-44 lg:h-80">
+                <div class="flex px-1 lg:w-80 lg:justify-between">
+                    <div class="mr-2 ml-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-facebook text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-twitter text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-black rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-linkedin-in text-white text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10 "><i class="fa-brands fa-instagram text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="pl-3 text-sm  lg:mt-2 mt-2">
+            <h1>Victor VIvas <br>
+               <span class="span">UI/UX designer</span> </h1>
+        </div>
+    </div>
+
+    <div class="photoContainer w-56 rounded-md shadow-md shadow-gray-400 h-60 relative group lg:w-1/2 lg:h-96 ml-4">
+        
+        <div>
+            <img src="/businessmanlarge2.jpg" alt="" class="w-full rounded-md h-44 lg:h-80">
+            <div class="hoverContainer absolute inset-0 rounded-md opacity-0 group-hover:opacity-75 flex items-center justify-center transition-opacity h-44 lg:h-80">
+                <div class="flex px-1 lg:w-80 lg:justify-between">
+                    <div class="mr-2 ml-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-facebook text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-twitter text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-black rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-linkedin-in text-white text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10 "><i class="fa-brands fa-instagram text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="pl-3 text-sm  lg:mt-2  mt-2">
+            <h1>Victor VIvas <br>
+               <span class="span">UI/UX designer</span> </h1>
+        </div>
+    </div>
+</div>
+<div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10">
+    <div class="photoContainer w-56 rounded-sm shadow-md shadow-gray-400 h-60 relative group lg:w-1/2 lg:h-96 ">
+        
+        <div>
+            <img src="/businesslarge3.jpg" alt="" class="w-full rounded-md h-44 lg:h-80">
+            <div class="hoverContainer absolute inset-0 rounded-md opacity-0 group-hover:opacity-75 flex items-center justify-center transition-opacity h-44 lg:h-80">
+                <div class="flex px-1 lg:w-80 lg:justify-between">
+                    <div class="mr-2 ml-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-facebook text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-twitter text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-black rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-linkedin-in text-white text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10 "><i class="fa-brands fa-instagram text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="pl-3 text-sm  lg:mt-2 mt-2">
+            <h1>Victor VIvas <br>
+               <span class="span">UI/UX designer</span> </h1>
+        </div>
+    </div>
+
+    <div class="photoContainer w-56 rounded-sm shadow-md shadow-gray-400 h-60 relative group lg:w-1/2 lg:h-96 ml-4">
+        
+        <div>
+            <img src="/businessmanlarge4.jpg" alt="" class="w-full rounded-md h-44 lg:h-80">
+            <div class="hoverContainer absolute inset-0 rounded-md opacity-0 group-hover:opacity-75 flex items-center justify-center transition-opacity h-44 lg:h-80">
+                <div class="flex px-1 lg:w-80 lg:justify-between">
+                    <div class="mr-2 ml-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-facebook text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-twitter text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-black rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-linkedin-in text-white text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10 "><i class="fa-brands fa-instagram text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="pl-3 text-sm  lg:mt-2 mt-2">
+            <h1>Victor VIvas <br>
+               <span class="span">UI/UX designer</span> </h1>
+        </div>
+    </div>
+</div>
+<div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10">
+    <div class="photoContainer w-56 rounded-sm shadow-md shadow-gray-400 h-60 relative group lg:w-1/2 lg:h-96 ">
+        
+        <div>
+            <img src="/businesslarge5.jpg" alt="" class="w-full rounded-md h-44 lg:h-80">
+            <div class="hoverContainer absolute inset-0 rounded-md opacity-0 group-hover:opacity-75 flex items-center justify-center transition-opacity h-44 lg:h-80">
+                <div class="flex px-1 lg:w-80 lg:justify-between">
+                    <div class="mr-2 ml-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-facebook text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-twitter text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-black rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-linkedin-in text-white text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10 "><i class="fa-brands fa-instagram text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="pl-3 text-sm  lg:mt-2 mt-2">
+            <h1>Manager Partner<br>
+               <span class="span">Architect</span> </h1>
+        </div>
+    </div>
+
+    <div class="photoContainer w-56 rounded-sm shadow-md shadow-gray-400 h-60 relative group lg:w-1/2 lg:h-96 ml-4">
+        
+        <div>
+            <img src="/businessmanlarge6.jpg" alt="" class="w-full rounded-md h-44 lg:h-80">
+            <div class="hoverContainer absolute inset-0 rounded-md opacity-0 group-hover:opacity-75 flex items-center justify-center transition-opacity h-44 lg:h-80">
+                <div class="flex px-1 lg:w-80 lg:justify-between">
+                    <div class="mr-2 ml-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-facebook text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-twitter text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-black rounded-full px-1 pt-1 lg:w-10 lg:h-10"><i class="fa-brands fa-linkedin-in text-white text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                    <div class="mr-2 items-center bg-white rounded-full px-1 pt-1 lg:w-10 lg:h-10 "><i class="fa-brands fa-instagram text-black text-2xl lg:text-2xl lg:ml-1 lg:mt-1"></i> </div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="pl-3 text-sm  lg:mt-2 mt-2">
+            <h1>Pedro Sanin<br>
+               <span class="span">Manager Partner</span> </h1>
+        </div>
+    </div>
+</div>
+    </div>
+  <Footer class="mt-20"/>
+  </template>
+  
+  <script>
+  import Footer from '@/components/Footer.vue'
+  import Navbar from '@/components/Navbar.vue'
+  export default {
+  components:{Footer,Navbar}
+  }
+  </script>
+  
+  <style>
+  .middleContainer{
+   background-color:#f0eafb
+  }
+  .lastContainer{
+    background-color: #E5DAFB;
+  }
+.photoContainer{
+    background-color:#debcf7d3
+}
+.span{
+    color:#5200e0;
+}
+.hoverContainer{
+    background-color: #00C7F9;
+}
+.button {
+    background: linear-gradient(to right, #9B4AF7 0%, #8523F5 100%);
+}
+  </style>
