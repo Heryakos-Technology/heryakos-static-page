3e617d26f94a449dce98b3632b6bb2b77d8f89cd
Tiegist Girma <tigistgirma40@gmail.com>
2025-03-01 02:15:14 +0300

    blog page implementaion first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BlogPage from '../views/BlogPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/BlogPage',
+      name: 'BlogPage',
+      component: BlogPage,
+    },
+    
     {
       path: '/about',
       name: 'about',
