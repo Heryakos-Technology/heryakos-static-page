9542e46af3ff17ddca0dd2020ab0f800eb9a9c84
complatform <kalkidansol1623@gmail.com>
2025-02-28 20:50:45 +0300

    make more corrections

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -17,7 +17,23 @@
             </div>
         </div>
     </div>
-    <div class="middleContainer mt-20 rounded-md shadow-sm flex lg:justify-between ml-3 mr-3 lg:ml-20 lg:mr-20">
+    <div class="lg:hidden mt-20">
+
+        <img src="/teamlarge2.jpg" alt="" class=" rounded-md h-84 lg:hidden relative" />
+        <div class="absolute middleContainer top-100 rounded-md shadow-sm ">
+            <div class="pl-4 pt-6 pb-5 lg:mt-5 lg:ml-10">
+            <h1 class="font-semibold lg:text-3xl">Welcome to Financial Company</h1>
+            <h2 class="mt-5 text-sm w-60 lg:w-2/3 lg:text-lg">
+                We are a passionate team dedicated to providing high-quality content and resources that inspire and inform our audience. With a focus on innovation and creativity, we strive to deliver engaging experiences that resonate with our visitors. Our mission is to connect, educate, and empower individuals through our carefully curated articles and insights.
+            </h2>
+            <div class="mt-6 lg:mt-5">
+                <a href="#" class="button text-white px-8 py-2 rounded-xl">KNOW MORE</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="hidden lg:block lg:visible">
+        <div class="middleContainer mt-20 rounded-md shadow-sm flex lg:justify-between ml-3 mr-3 lg:ml-20 lg:mr-20 ">
         <div class="pl-4 pt-6 pb-5 lg:mt-5 lg:ml-10">
             <h1 class="font-semibold lg:text-3xl">Welcome to Financial Company</h1>
             <h2 class="mt-5 text-sm w-60 lg:w-2/3 lg:text-lg">
@@ -27,11 +43,13 @@
                 <a href="#" class="button text-white px-8 py-2 rounded-xl">KNOW MORE</a>
             </div>
         </div>
-        <div class="w-full lg:w-full"> 
+        <div class=" lg:w-full"> 
             <img src="/teamsmall2.png" alt="" class="h-full rounded-md lg:hidden" />
             <img src="/teamlarge2.jpg" alt="" class="h-full w-full rounded-md hidden lg:block lg:visible lg:h-full lg:w-full lg:rounded-xl" />
         </div>
     </div>
+    </div>
+   
     <div class="lastContainer mt-20 rounded-sm shadow-sm pb-5 ml-3 mr-3 lg:ml-20 lg:mr-20 lg:pl-10 lg:pr-10">
         <h1 class="text-center font-semibold pt-5 lg:text-3xl">Our Teams</h1>
         <div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10">
