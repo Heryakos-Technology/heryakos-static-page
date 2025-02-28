0b784eac24434c22f720bad34d9ad604f96dcf42
complatform <kalkidansol1623@gmail.com>
2025-03-01 00:46:42 +0300

    mobile size implementation of contact us page

---
+++ src/views/ContactUs.vue
@@ -0,0 +1,171 @@
+<template>
+    <div class="mb-20">
+
+        <div class="mt-20">
+          <img src="/threeProples.png" alt="" class="max-w-full max-h-full mx-auto">
+        </div>
+        <div class="mt-8">
+            <h1 class="text-center text-4xl">Need a replay fast?</h1>
+            <h1 class="text-center text-6xl mt-12">Let's <span class="talk">talk!</span> </h1>
+            <p class="text-2xl ml-6 mt-10 font-light w-90 leading-9">Share your project requirements
+here or send us an  email at
+info@orizon.com we will follow up
+ in <span class="font-bold">24 hrs</span>  </p>
+        </div>
+        <div class="ml-8 mt-10 text-3xl font-light">
+            <div>
+                <div>
+                    <p class="text-2xl">Name</p>
+                </div>
+                <div class="mt-4">
+                    <input type="text" placeholder="Don from Earth,Inc." class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl ">
+                </div>
+            </div>
+            <div class="mt-16">
+                <div>
+                    <p class="text-2xl">Email </p>
+                </div>
+                <div class="mt-4">
+                    <input type="text" placeholder="don@norman.me" class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
+                </div>
+            </div>
+            <div class="mt-16">
+                <div>
+                    <p class="text-2xl">Phone Number </p>
+                </div>
+                <div class="mt-4">
+                    <input type="text" placeholder="(555) 555-555" class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
+                </div>
+            </div>
+            <div class="mt-16">
+                <div>
+                    <p class="text-2xl">Project Description </p>
+                </div>
+                <div class="mt-4">
+                    <textarea cols="24" rows="6" type="text" placeholder="We need to launch Earth’s  new UI and improve usability on key user flows such as climate, health and orbital escape velocity..." class="input border  border-pink-200 py-6 pl-6 pr-2 pt-12 pb-1 rounded-xl placeholder:text-black placeholder:text-2xl" style="white-space: pre-line;" />
+                </div>
+            </div>
+            <div class="mt-16">
+                <div>
+                    <p class="text-2xl">Subject </p>
+                </div>
+                <div class="mt-4">
+                    <input type="text" placeholder="enter your subject here." class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
+                </div>
+            </div>
+            <div class="mt-16 ml-40">
+                <button class="button text-white py-2 px-5 rounded-lg text-2xl ">Send Message</button>
+               
+            </div>
+            <div class="text-xl font-semibold ml-14 mt-8">
+                <h1> <h1 class="">⭐️ Join 100+ happy customers</h1></h1>
+            </div>
+            <div class="flex mt-20 ml-2">
+                <div>
+
+                    <hr class="text-gray-400 w-32">
+                </div>
+                <div>
+                    <h1 class="-mt-5 ml-4">or</h1>
+                </div>
+                <div class="">
+
+                    <hr class="text-gray-400 w-32 ml-4">
+                </div>
+            </div>
+            <div class="text-xl font-semibold w-80 mt-16">
+                <p>Filling out the form above is the
+fastest wat to get an answer,
+but you can also schedule a cal
+l in the upcoming days if you
+prefer.</p>
+            </div>
+        </div>
+        
+        <div class="ml-6 mt-24 bg-black text-white rounded-4xl pt-2 pb-5">
+            <div class="flex bg-black text-white rounded-4xl">
+                <div class="ml-4">
+                    <img src="/groupImageBottom.png" alt="">
+                </div>
+                <div class="pt-4 ml-4">
+                    <h1 class="font-semibold">Want to jump on  a <br>
+                        telegram chat?</h1>
+                    <p class="mt-2 text-xl font-light">Book a 30 minute 
+                      <br>  chat</p>
+                </div>
+                
+            </div>
+            <div class="ml-36 mt-5">
+                <button class="button text-white py-3 px-12 rounded-2xl text-2xl ">Continue</button>
+            </div>
+        </div>
+        <div class="mt-28">
+            <div class="flex ml-1 mr-1">
+<div class="flex cardDiv pl-3 pr-3 rounded-lg pt-3 pb-6 shadow-md">
+<div class="mr-2">
+    <i class="fa-solid fa-envelope text-black text-2xl"></i>
+</div>
+<div class="">
+    <h1 class="font-semibold">Email & Phone</h1>
+    <p class="text-sm text-gray-500 mt-2 font-semibold">yared123@gmail.com</p>
+    <p class="text-sm text-gray-500  font-semibold">+251916897173</p>
+</div>
+</div>
+<div class="flex cardDiv pl-3 pr-3 rounded-lg py-4 ml-3">
+<div class="mr-2">
+    <i class="fa-solid fa-location-dot text-black text-2xl"></i>
+</div>
+<div class="">
+    <h1 class="font-semibold ">Our Location</h1>
+    <p class="text-sm text-gray-500 font-semibold mt-2">Sherifa BLDG 8th
+floor,Meskel
+flowerAddid Ababa</p>
+
+</div>
+</div>
+            </div>
+            <div class="flex cardDiv pl-3 pr-2 w-48  rounded-lg pt-3 pb-7 ml-1 shadow-md mt-10">
+<div class="mr-2">
+    <i class="fa-solid fa-phone text-black text-2xl"></i>
+</div>
+<div class=" ">
+    <h1 class="font-semibold">Project Inquiry</h1>
+    <p class="text-sm text-gray-500 font-semibold mt-2">+251916897173</p>
+    <p class="text-sm text-gray-500 font-semibold">+251916897173</p>
+</div>
+</div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .talk {
+            font-size: 48px; 
+            background: linear-gradient(to right, 
+                #9B4AF7 0%,   
+                #8523F5 100%  
+            );
+            -webkit-background-clip: text; 
+            -webkit-text-fill-color: transparent; 
+            background-clip: text; 
+            text-fill-color: transparent; 
+        }
+        .input{
+            background-color:#ede4f0 ;
+        }
+    .button{
+        background: linear-gradient(to right, #9B4AF7 0%, #8523F5 100%);
+            
+    }
+    .cardDiv{
+        background: linear-gradient(to right, #D6C4F2 0%, #B693F5 100%);
+    }
+        
+</style> 
