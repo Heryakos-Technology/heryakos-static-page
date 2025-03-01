40a537c8f673e74af69aa9138a1491a5e3cb5fb0
complatform <kalkidansol1623@gmail.com>
2025-03-01 14:29:49 +0300

    adjesting tablet sizes

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -2,7 +2,7 @@
    
     <div class="mt-12 z-0">
         <div>
-            <img src="/team_photo1.png" alt="" class="h-68 relative lg:w-full lg:h-8/12" />
+            <img src="/team_photo1.png" alt="" class="h-68 relative lg:w-full lg:h-8/12 md:w-full" />
         </div>
         <div class="absolute top-28 left-5 lg:left-20 lg:top-56 text-white">
             <h1 class="lg:text-6xl lg:font-bold">FINANCIAL FOCUS <br />& CONSULTING</h1>
@@ -17,7 +17,7 @@
             </div>
         </div>
     </div>
-    <div class="lg:hidden mt-20">
+    <div class="lg:hidden mt-20 md:w-4/5 md:ml-28 md:mr-28">
 
         <img src="/teamlarge2.jpg" alt="" class=" rounded-md h-84 lg:hidden relative" />
         <div class="absolute middleContainer w-64 top-100 rounded-md shadow-sm ">
@@ -34,7 +34,7 @@
     </div>
     <div class="hidden lg:block lg:visible">
         <div class="middleContainer mt-20 rounded-md shadow-sm flex lg:justify-between ml-3 mr-3 lg:ml-32 lg:mr-32 ">
-        <div class="pl-4 pt-6 pb-5 lg:mt-5 lg:ml-10 lg:w-4/5">
+        <div class="pl-4 pt-6 pb-5 lg:mt-5 lg:ml-10 lg:w-4/5 md:w-full">
             <h1 class="font-semibold lg:text-3xl">Welcome to Financial Company</h1>
             <h2 class="mt-5 text-sm w-60 lg:w-4/5 lg:text-lg">
                 We are a passionate team dedicated to providing high-quality content and resources that inspire and inform our audience. With a focus on innovation and creativity, we strive to deliver engaging experiences that resonate with our visitors. Our mission is to connect, educate, and empower individuals through our carefully curated articles and insights.
@@ -50,9 +50,9 @@
     </div>
     </div>
    
-    <div class="lastContainer mt-20 rounded-sm shadow-sm pb-5 ml-3 mr-3 lg:ml-32 lg:mr-32 lg:pl-10 lg:pr-10">
+    <div class="lastContainer mt-20 rounded-sm shadow-sm pb-5 ml-3 mr-3 lg:ml-32 lg:mr-32 lg:pl-10 lg:pr-10 md:ml-28 md:mr-28">
         <h1 class="text-center font-semibold pt-5 lg:text-3xl">Our Teams</h1>
-        <div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10">
+        <div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10 md:ml-9 md:mr-9">
             <TeamCard 
                 name="Victor Vivas" 
                 role="UI/UX Designer" 
@@ -65,7 +65,7 @@
                 imgSrc="/PedroMale.jpg" 
             />
         </div>
-        <div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10">
+        <div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10 md:ml-9 md:mr-9">
             <TeamCard 
                 name="Maria José Jimenez" 
                 role="Architect" 
@@ -78,7 +78,7 @@
                 imgSrc="/MariaFemale.jpg" 
             />
         </div>
-        <div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10">
+        <div class="flex ml-6 mr-6 mt-6 lg:justify-between lg:ml-10 lg:mr-10 md:ml-9 md:mr-9">
             <TeamCard 
                 name="Maria José Jimenez" 
                 role="UI/UX Designer" 
