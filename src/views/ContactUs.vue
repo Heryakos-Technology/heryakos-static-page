9ebd0449370ee8de5b3fd6451d2fb2aa0b609f0d
complatform <kalkidansol1623@gmail.com>
2025-03-01 19:09:43 +0300

    making responsive

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,79 +1,110 @@
 <template>
-    <div class="mb-20">
+    <div class="mb-20 lg:w-2/3 lg:mx-auto  md:w-4/5 md:mx-auto">
 
-        <div class="mt-20">
-          <img src="/threeProples.png" alt="" class="max-w-full max-h-full mx-auto">
+        <div class="mt-20 md:">
+          <img src="/threeProples.png" alt="" class="max-w-full max-h-full mx-auto lg:w-96">
         </div>
-        <div class="mt-8">
-            <h1 class="text-center text-4xl">Need a reply fast?</h1>
+        <div class="mt-8 ">
+            <h1 class="text-center text-4xl lg:text-5xl">Need a reply fast?</h1>
             <h1 class="text-center text-6xl mt-12">Let's <span class="talk">talk!</span> </h1>
-            <p class="text-2xl ml-6 mt-10 font-light w-90 leading-9">Share your project requirements
+            <p class="text-2xl  mt-10 font-light w-5/6 mx-auto leading-9 lg:w-2/3 lg:mx-auto md:w-120 md:mx-auto">Share your project requirements
 here or send us an email at
 info@orizon.com we will follow up
  in <span class="font-bold">24 hrs</span>  </p>
         </div>
-        <div class="ml-8 mt-10 text-3xl font-light">
+        <div class="ml-8 mt-10 text-3xl font-light lg:mx-auto md:mx-auto">
             <div>
-                <div>
-                    <p class="text-2xl">Name</p>
+                <div class="">
+                    <p class="text-2xl lg:text-xl">Name</p>
                 </div>
                 <div class="mt-4">
-                    <input type="text" placeholder="Don from Earth,Inc." class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl ">
+                    <input type="text" placeholder="Don from Earth,Inc." class="input border w-13/14 border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl lg:w-7/8">
                 </div>
             </div>
-            <div class="mt-16">
+            <div class="hidden lg:block mt-16">
+                <div class="flex w-full">
+                    <div class="mr-5">
+                        <div class="">
+                    <p class="text-2xl lg:text-xl">Email</p>
+                </div>
+                <div class="mt-4 mr-2">
+                    <input type="text" placeholder="don@norman.me" class="input border  border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl ">
+                </div>
+                    </div>
+                    <div class="ml-22">
+                        <div class="">
+                    <p class="text-2xl lg:text-xl">Phone</p>
+                </div>
+                <div class="mt-4 ">
+                    <input type="text" placeholder="(555) 555-555" class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
+                </div>
+                    </div>
+                 
+                </div>
+              
+            </div>
+            <div class="mt-16 lg:hidden">
                 <div>
                     <p class="text-2xl">Email </p>
                 </div>
                 <div class="mt-4">
-                    <input type="text" placeholder="don@norman.me" class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
+                    <input type="text" placeholder="don@norman.me" class="input border w-13/14 border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
                 </div>
             </div>
-            <div class="mt-16">
+            <div class="mt-16 lg:hidden">
                 <div>
                     <p class="text-2xl">Phone Number </p>
                 </div>
                 <div class="mt-4">
-                    <input type="text" placeholder="(555) 555-555" class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
+                    <input type="text" placeholder="(555) 555-555" class="input border w-13/14 border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
                 </div>
             </div>
             <div class="mt-16">
                 <div>
-                    <p class="text-2xl">Project Description </p>
+                    <p class="text-2xl lg:text-xl">Project Description </p>
                 </div>
-                <div class="mt-4">
-                    <textarea cols="24" rows="6" type="text" placeholder="We need to launch Earth’s new UI and improve usability on key user flows such as climate, health and orbital escape velocity..." class="input border  border-pink-200 py-6 pl-6 pr-2 pt-12 pb-1 rounded-xl placeholder:text-black placeholder:text-2xl" style="white-space: pre-line;" />
+                <div class="mt-4 lg:hidden">
+                    <textarea cols="24" rows="6"  type="text" placeholder="We need to launch Earth’s new UI and improve usability on key user flows such as climate, health and orbital escape velocity..." class="input border  border-pink-200 py-6 w-13/14 pl-6 pr-2 pt-12 pb-1 rounded-xl placeholder:text-black placeholder:text-2xl " style="white-space: pre-line;" />
+                </div>
+                <div class="mt-4 hidden lg:block">
+                    <textarea cols="49" rows="5"  type="text" placeholder="We need to launch Earth’s new UI and improve usability on key user flows such as climate, health and orbital escape velocity..." class="input border  border-pink-200 py-6 pl-6 pr-2 lg:pt-16 lg:w-7/8 lg:pb-0 pt-12 pb-1 rounded-xl placeholder:text-black placeholder:text-2xl " style="white-space: pre-line;" />
                 </div>
             </div>
             <div class="mt-16">
                 <div>
-                    <p class="text-2xl">Subject </p>
+                    <p class="text-2xl lg:text-xl">Subject </p>
                 </div>
                 <div class="mt-4">
-                    <input type="text" placeholder="enter your subject here." class="input border border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl">
+                    <input type="text" placeholder="enter your subject here." class="input border w-13/14 border-pink-200 py-6 pl-6 rounded-xl placeholder:text-black placeholder:text-2xl lg:w-7/8">
                 </div>
             </div>
-            <div class="mt-16 ml-40">
-                <button class="button text-white py-2 px-5 rounded-lg text-2xl ">Send Message</button>
+            <div class="mt-16 ml-40 lg:ml-156 justify-end" >
+                <button class="button text-white py-2 px-5 w-10/11 -ml-3    rounded-lg text-2xl lg:rounded-3xl lg:text-lg lg:px-8 lg:-ml-40">Send Message</button>
                
             </div>
-            <div class="text-xl font-semibold ml-14 mt-8">
-                <h1> <h1 class="">⭐️ Join 100+ happy customers</h1></h1>
+            <div class="text-xl   font-semibold  mt-8 lg:mx-auto">
+                 <h1 class="w-4/5 ml-12 md:w-1/2 md:ml-auto">⭐️ Join 100+ happy customers</h1>
             </div>
-            <div class="flex mt-20 ml-2">
-                <div>
+            <div class="flex mt-20 mx-auto lg:mx-auto lg:mr-20">
+                <!-- <div>
 
-                    <hr class="text-gray-400 w-32">
+                    <hr class="text-gray-400 w-40 lg:w-76  md:w-70">
+                </div> -->
+                <div class="bg-gray-400 w-40 h-0.5 md:w-1/2 ">
+
                 </div>
                 <div>
-                    <h1 class="-mt-5 ml-4">or</h1>
+                    <h1 class="-mt-5 mx-1">or</h1>
                 </div>
-                <div class="">
+                <!-- <div class="">
 
-                    <hr class="text-gray-400 w-32 ml-4">
-                </div>
+                    <hr class="text-gray-400 w-40 lg:w-76 md:w-70">
+                </div> -->
+                <div class="bg-gray-400 w-40 h-0.5 md:w-1/2 ">
+
+</div>
             </div>
-            <div class="text-xl font-semibold w-80 mt-16">
+            <div class="text-xl font-semibold w-6/7 mx-auto mt-16 lg:w-3/4 lg:mx-auto">
                 <p>Filling out the form above is the
 fastest way to get an answer,
 but you can also schedule a cal
@@ -82,56 +113,74 @@ prefer.</p>
             </div>
         </div>
         
-        <div class="ml-2 mt-24 bg-black text-white rounded-4xl pt-2 pb-5">
-            <div class="flex bg-black text-white rounded-4xl">
+        <div class="ml-2 mt-24 bg-black text-white rounded-4xl pt-2 pb-5 lg:hidden">
+            <div class="flex bg-black text-white rounded-4xl lg:rounded-full lg:hidden">
                 <div class="ml-4">
                     <img src="/groupImageBottom.png" alt="">
                 </div>
-                <div class="pt-4 ml-4">
-                    <h1 class="font-semibold">Want to jump on  a <br>
+                <div class="pt-4 ml-4 lg:mt-5">
+                    <h1 class="font-semibold">Want to jump on  a <br class="lg:hidden">
                         telegram chat?</h1>
                     <p class="mt-2 text-xl font-light">Book a 30 minute 
-                      <br>  chat</p>
+                      <br class="lg:hidden">  chat</p>
                 </div>
                 
             </div>
             <div class="ml-36 mt-5">
-                <button class="button text-white py-3 px-12 rounded-2xl text-2xl ">Continue</button>
+                <button class="button text-white py-3 px-12 rounded-2xl text-2xl">Continue</button>
             </div>
         </div>
-        <div class="mt-28">
+        <div class="hidden lg:block text-white  bg-black mt-20 pr-10 pl-16 rounded-full pb-10 pt-5  ">
+<div class="flex">
+<div>
+    <img src="/groupImageBottom.png" alt="">
+</div>
+<div>
+    <div class="pt-4 ml-4 lg:mt-2">
+                    <h1 class="font-semibold">Want to jump on  a <br class="lg:hidden">
+                        telegram chat?</h1>
+                    <p class="mt-2 text-xl font-light">Book a 30 minute 
+                      <br class="lg:hidden">  chat</p>
+                </div>
+</div>
+<div class="ml-36 mt-5">
+                <button class="button text-white py-3 px-12 rounded-full mt-8 -ml-30 text-2xl">Continue</button>
+            </div>
+</div>
+        </div>
+        <div class="mt-28 ">
             <div class="flex ml-1 mr-1">
-                <div class="flex cardDiv pl-3 pr-3 rounded-lg pt-3 pb-6 shadow-md">
+                <div class="flex cardDiv pl-3 pr-3 w-3/4 rounded-lg pt-3 pb-6 shadow-md lg:w-4/5 lg:mr-5 lg:pl-10 lg:pt-6">
                     <div class="mr-2">
-                        <i class="fa-solid fa-envelope text-black text-2xl"></i>
+                        <i class="fa-solid fa-envelope text-black text-2xl lg:text-4xl"></i>
                     </div>
-                    <div class="">
-                        <h1 class="font-semibold">Email & Phone</h1>
-                        <p class="text-sm text-gray-500 mt-2 font-semibold">yared123@gmail.com</p>
-                        <p class="text-sm text-gray-500  font-semibold">+251916897173</p>
+                    <div class="lg:ml-5">
+                        <h1 class="font-semibold lg:text-xl">Email & Phone</h1>
+                        <p class="text-sm text-gray-500 mt-2 font-semibold lg:text-lg">yared123@gmail.com</p>
+                        <p class="text-sm text-gray-500  font-semibold lg:text-lg">+251916897173</p>
                     </div>
                 </div>
-                <div class="flex cardDiv pl-3 pr-3 rounded-lg py-4 ml-3">
+                <div class="flex cardDiv w-3/4 pl-3 pr-3 rounded-lg py-4 ml-3 lg:w-4/5 lg:h-48 lg:mr-5 lg:pl-10 lg:pt-6">
                     <div class="mr-2">
-                        <i class="fa-solid fa-location-dot text-black text-2xl"></i>
+                        <i class="fa-solid fa-location-dot text-black text-2xl lg:text-4xl"></i>
                     </div>
-                    <div class="">
-                        <h1 class="font-semibold ">Our Location</h1>
-                        <p class="text-sm text-gray-500 font-semibold mt-2">Sherifa BLDG 8th
+                    <div class="lg:ml-5">
+                        <h1 class="font-semibold lg:text-xl">Our Location</h1>
+                        <p class="text-sm text-gray-500 font-semibold mt-2 lg:text-lg">Sherifa BLDG 8th
 floor,Meskel
 flowerAddid Ababa</p>
 
                     </div>
                 </div>
             </div>
-            <div class="flex cardDiv pl-3 pr-2 w-48  rounded-lg pt-3 pb-7 ml-1 shadow-md mt-10">
+            <div class="flex cardDiv w-1/2 pl-3 pr-2  rounded-lg pt-3 pb-7 ml-1 shadow-md mt-10 lg:h-48 lg:w-107 lg:pr-1 lg:pl-10 lg:pt-6">
                 <div class="mr-2">
-                    <i class="fa-solid fa-phone text-black text-2xl"></i>
+                    <i class="fa-solid fa-phone text-black text-2xl lg:text-4xl mt-2"></i>
                 </div>
-                <div class=" ">
-                    <h1 class="font-semibold">Project Inquiry</h1>
-                    <p class="text-sm text-gray-500 font-semibold mt-2">+251916897173</p>
-                    <p class="text-sm text-gray-500 font-semibold">+251916897173</p>
+                <div class="lg:ml-5">
+                    <h1 class="font-semibold lg:text-xl">Project Inquiry</h1>
+                    <p class="text-sm text-gray-500 font-semibold mt-2 lg:text-lg">+251916897173</p>
+                    <p class="text-sm text-gray-500 font-semibold lg:text-lg">+251916897173</p>
                 </div>
             </div>
         </div>
