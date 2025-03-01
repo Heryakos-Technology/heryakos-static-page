1376cfce0e8de1337edf84d5c3f0e477be068492
complatform <kalkidansol1623@gmail.com>
2025-03-01 14:11:29 +0300

    adding vite config file

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production'
-  ? '/heryakos-static-page/'
-  : '/',
 })
+
+
+///
 
