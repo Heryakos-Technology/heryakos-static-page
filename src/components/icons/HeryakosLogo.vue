fc998580fe6f369157a66ea7558d1a665086da54
complatform <kalkidansol1623@gmail.com>
2025-03-03 21:23:28 +0300

    add nave bar

---
+++ src/components/icons/HeryakosLogo.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Logo from '/public/Logo.png'
+const props = defineProps({
+  class: String,
+})
+</script>
+
+<template>
+  <img :src="Logo" alt="Heryakos Logo" :class="props.class">
+</template>
