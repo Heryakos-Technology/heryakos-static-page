f020e6b81133f779b1efc0d5cf712fffbe9b9251
complatform <kalkidansol1623@gmail.com>
2025-03-04 20:41:00 +0300

    corrections

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
+//import { library } from '@fortawesome/fontawesome-svg-core';
 
 import App from './App.vue'
 import router from './router'
