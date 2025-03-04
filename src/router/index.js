42b31e40b7f0ac4acf5ee961a9d730f46c8d2588
Tiegist Girma <tigistgirma40@gmail.com>
2025-03-04 22:27:28 +0300

    trying

--- src/router/index.js
+++ src/router/index.js
@@ -12,12 +12,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/contact',
+      path: '/',
       name: 'ContactUs',
       component: ContactUs,
     },
