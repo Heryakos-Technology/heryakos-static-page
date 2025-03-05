6e05aca28bceafd311f1ea82af97ef22b92b91e3
Tiegist Girma <tigistgirma40@gmail.com>
2025-03-06 02:12:00 +0300

    service page modification

--- src/views/ServicePage.vue
+++ src/views/ServicePage.vue
@@ -1,19 +1,19 @@
 <template>
 
   <div>
-    <!-- mobile size -->
-    <div class="block md:hidden md:mb-64">
-      <img
-        class="w-11/12 md:w-11/12 md:mx-auto md:h-screen md:overflow-hidden mx-auto h-72 mt-6 rounded-xl relative text-purple-300"
-        src="/herosection-service.png" alt="Service Landing Image">
-      <p
-        class="md:h-1/3 md:relative text-md -mt-72 pt-2 w-full text-center mx-auto  absolute font-sans font-bold bg-gradient-to-r from-[#984AF7] to-[#8523F5] bg-clip-text text-transparent">
+ 
+    <div class="block md:hidden mb-36 mt-16">
+        <img src="/intro-service.jpg" alt="" class="w-4/5 ml-16 mt-5 relative ">
+        <div class="w-11/12 h-44 bg-white absolute -mt-14">
+          <p
+        class="w-full text-center  font-bold bg-gradient-to-r from-[#984AF7] to-[#8523F5] bg-clip-text text-transparent">
         OUR SERVICES
       </p>
-      <p class="w-full px-20 text-center p-2 absolute -mt-64 mx-auto text-xs font-extralight text-white">We offer
+            <p class="text-wrap text-sm ml-2 pt-5 pl-5 pr-4  ">We offer
         cutting-edge technology solutions designed to accelerate your business growth. From stunning web experiences to
         powerful mobile applications and future-ready blockchain solutions, we provide end-to-end services tailored to
         your unique needs.</p>
+        </div>
     </div>
     <!-- /end of mobile size -->
 
@@ -21,7 +21,7 @@
     <div class="block md:hidden">
       <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] md:p-8 w-11/12 pb-2 rounded-lg mx-auto mt-8 mb-10 ">
         <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px;" src="/first-service.png"
-          alt="Static Website Development" class="mx-auto rounded-bl-2xl rounded-tr-full w-full h-auto">
+          alt="Static Website Development" class="mx-auto rounded-bl-2xl rounded-tr-full w-full h-64">
         <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center mt-4">
           Static Website Development
         </h2>
@@ -36,7 +36,7 @@
 
       <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] md:p-8 w-11/12 pb-2 rounded-lg mx-auto mt-8 mb-2">
         <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px;" src="/second-service.png"
-          alt="Static Website Development" class="mx-auto rounded-br-2xl rounded-tl-full w-full h-auto">
+          alt="Static Website Development" class="mx-auto rounded-br-2xl rounded-tl-full w-full h-64">
         <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center mt-4">
           Technology Consultancy
         </h2>
@@ -51,7 +51,7 @@
 
       <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] md:p-8 w-11/12 pb-2 rounded-lg mx-auto mt-8 mb-10">
         <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px;" src="/third-service.png"
-          alt="Static Website Development" class="mx-auto rounded-bl-2xl rounded-tr-full w-full h-auto">
+          alt="Static Website Development" class="mx-auto rounded-bl-2xl rounded-tr-full w-full h-64">
         <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center mt-4">
           Mobile Application Development (Android & iOS)
         </h2>
@@ -67,7 +67,7 @@
 
       <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] md:p-8 w-11/12 pb-2 rounded-lg mx-auto mt-8 mb-2">
         <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px;" src="/fourth-servicee.png"
-          alt="Static Website Development" class="mx-auto rounded-br-2xl rounded-tl-full w-full h-auto">
+          alt="Static Website Development" class="mx-auto rounded-br-2xl rounded-tl-full w-full h-64">
         <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center mt-4">
           Dynamic Website Development
         </h2>
@@ -82,7 +82,7 @@
 
       <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] md:p-8 w-11/12 pb-2 rounded-lg mx-auto mt-8 mb-10">
         <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px;" src="/fifth-service.png"
-          alt="Static Website Development" class="mx-auto rounded-bl-2xl rounded-tr-full w-full h-auto">
+          alt="Static Website Development" class="mx-auto rounded-bl-2xl rounded-tr-full w-full h-64">
         <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center mt-4">
           Blockchain Integration
         </h2>
@@ -97,7 +97,7 @@
 
       <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] md:p-8 w-11/12 pb-2 rounded-lg mx-auto mt-8 mb-10">
         <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px;" src="/sixth-service.png"
-          alt="Static Website Development" class="mx-auto rounded-br-2xl rounded-tl-full w-full h-auto">
+          alt="Static Website Development" class="mx-auto rounded-br-2xl rounded-tl-full w-full h-64">
         <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center mt-4">
           Cloud Computing Solutions
         </h2>
@@ -114,43 +114,62 @@
 
     <!-- desktop -->
     <div class="hidden lg:block md:block">
-      <div class="lg:mb-96 md:mb-64">
+      <div class=" md:mb-64 ml-40">
         <img
-          class="w-11/12 lg:w-11/12 md:w-11/12 md:ml-8 lg:mx-auto lg:h-screen lg:overflow-hidden ml-4 h-72 mt-6 rounded-4xl relative text-purple-300"
-          src="/herosection-service.png" alt="Service Landing Image">
-        <div class="-mt-64 lg:-mt-96 w-64 mx-auto">
-          <p
-            class="lg:text-5xl lg:text-center lg:-ml-14 text-md md:mt-4 md:ml-20 lg:-mt-64 pt-2 absolute font-sans font-bold bg-gradient-to-r from-[#984AF7] to-[#8523F5] bg-clip-text text-transparent">
-            OUR SERVICES
-          </p>
-        </div>
-        <div class="w-2/3 mx-auto -mt-96 md:mt-20">
-          <p class="w-1/2 text-center p-2 absolute lg:text-2xl ml-20 lg:-mt-64 font-extralight text-white md:text-sm">We offer
-            cutting-edge technology solutions designed to accelerate your business growth. From stunning web experiences
-            to powerful mobile applications and future-ready blockchain solutions, we provide end-to-end services
-            tailored to your unique needs.</p>
-        </div>
+          class=" pt-32 ml-96    relative text-purple-300"
+          src="/intro-service.jpg" alt="Service Landing Image" >
+          <div class="w-2/6 -ml- h-88 -mt-96 bg-white absolute">
+            <div class="">
+              <p
+                class="lg:text-3xl text-center text-md md:mt-4   pt-2  font-sans font-bold bg-gradient-to-r from-[#984AF7] to-[#8523F5] bg-clip-text text-transparent">
+                OUR SERVICES
+              </p>
+            </div>
+            <div class="w-11/12 mx-auto ">
+              <p class=" text-center p-2  lg:text-2xl font-semi-bold text-black md:text-sm">We offer
+                cutting-edge technology solutions designed to accelerate your business growth. From stunning web experiences
+                to powerful mobile applications and future-ready blockchain solutions, we provide end-to-end services
+                tailored to your unique needs.</p>
+            </div>
+          </div>
       </div>
 
-      <div class="flex  lg:px-20 md:px-4 md:-mt-20">
-        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/2 md:w-1/2 md:mr-2 lr:mr-5 rounded-lg h-1/3 mx-auto mt-8 lg:pb-8  mb-10">
-          <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px; height: 450px;"
+      <div class="flex  lg:px-20 md:px-4  -mt-60">
+        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/3 md:w-1/2 md:mr-2 lg:mr-10 rounded-lg h-1/3 mx-auto mt-8 lg:pb-8  mb-10">
+          <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px; height: 300px;"
             src="/first-service.png" alt="Static Website Development"
-            class="mx-auto rounded-bl-2xl rounded-tr-full w-full mb-6">
+            class="mx-auto   w-full mb-6">
           <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center">
             Static Website Development
           </h2>
           <p class="text-lg text-center font-serif">
             <span class="text-blue-500">🌐</span> "Beautifully Crafted Websites That Speak for Your Brand"
           </p>
-          <p class="ml-16 mt-10 mb-4 w-4/5 text-center pb-6">
+          <p class="mt-10  w-4/5 mx-auto text-center pb-">
             We design fast, secure, visually stunning static websites ideal for portfolios, landing pages, and
             informational platforms. Our static websites ensure top performance with minimal maintenance.
           </p>
         </div>
 
-        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/2 md:w-1/2 md:ml-4 lg:ml-5 rounded-lg h-1/3 mx-auto mt-8 lg:mb-1 md:pb-8">
-          <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px; height: 450px;"
+
+        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/3 md:w-1/2 lg:mr-5 md:mr-2 h-1/3 rounded-lg mx-auto mt-8 mb-10">
+          <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px; height: 300px;"
+            src="/third-service.png" alt="Static Website Development" class="mx-auto w-full mb-6">
+          <h2 class="text-2xl md:text-3xl font-normal mb-4 w-full mx-auto text-center">
+            Mobile Application Development (Android & iOS)
+          </h2>
+          <p class="text-lg text-center font-serif mb-2">
+            <span class="text-blue-500">📱</span> "Seamless Mobile Experiences at Your Fingertips"
+          </p>
+          <p class="mb-4 w-4/5 mx-auto mt-6 pb-6 text-center">
+            Reach a wider audience with intuitive and responsive mobile applications. We specialize in building
+            high-performance Android and iOS apps with offline capabilities and real-time data sync for maximum user
+            engagement.
+          </p>
+        </div>
+
+        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/3 md:w-1/2 md:ml-4 lg:ml-5 rounded-lg h-1/3 mx-auto mt-8 lg:mb-1 md:pb-8">
+          <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px; height: 300px;"
             src="/second-service.png" alt="Static Website Development" class="mx-auto w-full h-1/2 mb-6">
           <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center">
             Technology Consultancy
@@ -158,32 +177,19 @@
           <p class="text-lg text-center font-serif mb-2">
             <span class="text-blue-500">🔍</span> "Empowering Ideas with the Right Technology"
           </p>
-          <p class="ml-16 mb-4 w-4/5 mt-10 pb-6 text-center">
+          <p class=" mb-4 w-4/5 mx-auto mt-10 pb-3 text-center">
             We provide expert guidance to help you choose the right technologies and strategies for your business. From
             project planning to tech stack selection, we ensure you make informed decisions that lead to success.
           </p>
         </div>
       </div>
 
-      <div class="flex lg:px-20 md:px-4 -mt-8">
-        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/2 md:w-1/2 lg:mr-5 md:mr-2 h-1/3 rounded-lg mx-auto mt-8 mb-10">
-          <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px; height: 450px;"
-            src="/third-service.png" alt="Static Website Development" class="mx-auto w-full mb-6">
-          <h2 class="text-2xl md:text-3xl font-normal mb-4 w-4/5 mx-auto text-center">
-            Mobile Application Development (Android & iOS)
-          </h2>
-          <p class="text-lg text-center font-serif mb-2">
-            <span class="text-blue-500">📱</span> "Seamless Mobile Experiences at Your Fingertips"
-          </p>
-          <p class="ml-16 mb-4 w-4/5 mt-10 pb-6 text-center">
-            Reach a wider audience with intuitive and responsive mobile applications. We specialize in building
-            high-performance Android and iOS apps with offline capabilities and real-time data sync for maximum user
-            engagement.
-          </p>
-        </div>
+ 
 
-        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/2 md:w-1/2 lg:ml-5 md:ml-4 h-1/3 rounded-lg pb-9 mx-auto mt-8 mb-10">
-          <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px; height: 450px;"
+      <div class="flex lg:px-20 md:px-4 -mt-8">
+
+        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/3 md:w-1/2 lg:ml-5 md:ml-4 h-1/3 rounded-lg pb-9 mx-auto mt-8 mb-10">
+          <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px; height: 300px;"
             src="/fourth-servicee.png" alt="Static Website Development" class="mx-auto w-full mb-6">
           <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center">
             Dynamic Website Development
@@ -191,31 +197,15 @@
           <p class="text-lg text-center font-serif mb-2">
             <span class="text-blue-500">⚡</span> "Interactive Web Solutions That Engage and Convert"
           </p>
-          <p class="ml-16 mb-4 w-4/5 mt-10 pb-6 text-center">
+          <p class=" mb-4 w-4/5 mx-auto  text-center">
             Transform your online presence with feature-rich, dynamic websites that adapt to user interactions. We build
             scalable platforms with powerful backends tailored for e-commerce, blogs, and web applications.
           </p>
         </div>
-      </div>
+        
 
-      <div class="flex lg:px-20 md:px-4 -mt-8">
-        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/2 md:w-1/2 lg:mr-5 md:mr-2 h-1/3 rounded-lg mx-auto mt-8 mb-10">
-          <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px; height: 450px;"
-            src="/fifth-service.png" alt="Static Website Development" class="mx-auto w-full mb-6">
-          <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center">
-            Blockchain Integration
-          </h2>
-          <p class="text-lg text-center font-serif mb-2">
-            <span class="text-blue-500">🔗 </span> "Future-Proof Your Business with Blockchain"
-          </p>
-          <p class="ml-16 mb-4 w-4/5 mt-10 pb-6 text-center">
-            Leverage the power of blockchain for secure transactions, smart contracts, and decentralized applications.
-            We integrate blockchain solutions tailored to your business needs, ensuring transparency and trust.
-          </p>
-        </div>
-
-        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/2 md:w-1/2 h-1/3 lg:ml-5 md:ml-4 rounded-lg mx-auto mt-8 mb-10">
-          <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px; height: 450px;"
+        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/3  md:w-1/2 h-1/3 lg:mr-5 lg:ml-5 md:ml-4 rounded-lg mx-auto mt-8 mb-10">
+          <img style="border-top-left-radius: 130px; border-bottom-right-radius: 130px; height: 300px;"
             src="/sixth-service.png" alt="Static Website Development" class="mx-auto w-full mb-6">
           <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center">
             Cloud Computing Solutions
@@ -223,9 +213,24 @@
           <p class="text-lg text-center font-serif mb-2">
             <span class="text-gray-500">☁ </span> "Scalable, Secure, and Reliable Cloud Solutions"
           </p>
-          <p class="ml-16 mb-4 w-4/5 mt-10 pb-6 text-center">
+          <p class="  w-4/5 mx-auto mt-10 pb-12 text-center">
             Optimize your business operations with our cloud computing services. We offer cloud integration, migration,
             and management solutions that ensure scalability, data security, and high availability.
+          </p>
+        </div>
+
+        <div class=" bg-gradient-to-b from-[#F7F3FE] to-[#eee6ff] lg:w-1/3 md:w-1/2 lg:mr-5 md:mr-2 h-1/3 rounded-lg mx-auto mt-8 mb-10">
+          <img style="border-top-right-radius: 130px; border-bottom-left-radius: 130px; height: 300px;"
+            src="/fifth-service.png" alt="Static Website Development" class="mx-auto w-full mb-6">
+          <h2 class="text-2xl md:text-3xl font-normal mb-4 text-center">
+            Blockchain Integration
+          </h2>
+          <p class="text-lg text-center font-serif mb-2">
+            <span class="text-blue-500">🔗 </span> "Future-Proof Your Business with Blockchain"
+          </p>
+          <p class=" mb-4 w-4/5 mx-auto mt-10 pb-8 text-center">
+            Leverage the power of blockchain for secure transactions, smart contracts, and decentralized applications.
+            We integrate blockchain solutions tailored to your business needs, ensuring transparency and trust.
           </p>
         </div>
       </div>
@@ -432,10 +437,7 @@
       </div>
     </div>
     <!-- /end of desktop view -->
-    <div>
-      <p class="text-md lg:text-2xl mb-4 w-full font-bold text-center  ">Ready to Showcase Your Products? <a href="#"
-          class="text-[#984AF7]"><u>Click here</u></a> to sign up!</p>
-    </div>
+
   </div>
 
 </template>
