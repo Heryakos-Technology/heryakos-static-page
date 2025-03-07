3f9f929e587a9129932ceb030de8ef0889489937
RohaTech <raklovetocode@gmail.com>
2025-03-07 10:00:24 +0300

    Revert "refactor: remove unnecessary blank lines in main.js"
    This reverts commit be5ebcd15bc5bdbc9b7687a8c206e8be922aa1ce.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-//import { library } from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 import App from './App.vue'
 import router from './router'
@@ -11,4 +11,5 @@ const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia())
 app.use(router)
+
 app.mount('#app')
