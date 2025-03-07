be5ebcd15bc5bdbc9b7687a8c206e8be922aa1ce
RohaTech <raklovetocode@gmail.com>
2025-03-07 09:41:16 +0300

    refactor: remove unnecessary blank lines in main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-
 import App from './App.vue'
 import router from './router'
 
@@ -12,5 +11,4 @@ const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
