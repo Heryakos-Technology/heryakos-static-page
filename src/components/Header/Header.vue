936e457beb339c9b519eeddc818cdcab0c7b54ce
RohaTech <raklovetocode@gmail.com>
2025-03-11 22:01:55 +0300

    Refactor logo components and update header styles for improved responsiveness

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -6,9 +6,7 @@ import { RouterLink, useRoute } from "vue-router";
 
 const route = useRoute();
 
-
 const isSidebarOpen = ref(false);
-
 
 const isNavbarVisible = ref(true);
 let lastScrollPosition = 0;
@@ -17,7 +15,6 @@ const handleSidebarOpen = () => {
   isSidebarOpen.value = !isSidebarOpen.value;
   console.log(isSidebarOpen.value);
 };
-
 
 const navLinks = [
   { name: "Home", label: "Home" },
@@ -30,17 +27,17 @@ const navLinks = [
 const handleScroll = () => {
   const currentScrollPosition = window.scrollY;
 
-  if (currentScrollPosition > lastScrollPosition && currentScrollPosition > 30) {
-
+  if (
+    currentScrollPosition > lastScrollPosition &&
+    currentScrollPosition > 30
+  ) {
     isNavbarVisible.value = false;
   } else if (currentScrollPosition < lastScrollPosition) {
-
     isNavbarVisible.value = true;
   }
 
   lastScrollPosition = currentScrollPosition;
 };
-
 
 onMounted(() => {
   window.addEventListener("scroll", handleScroll);
@@ -50,11 +47,9 @@ onUnmounted(() => {
   window.removeEventListener("scroll", handleScroll);
 });
 
-
 watch(isSidebarOpen, (newValue) => {
   document.body.style.overflow = newValue ? "hidden" : "auto";
 });
-
 
 onUnmounted(() => {
   document.body.style.overflow = "auto";
@@ -62,27 +57,40 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="w-full md:fixed z-50 md:top-5 transition-transform duration-300 ease-in-out"
-    :class="{ '-translate-y-full md:-translate-y-[calc(100%+20px)]': !isNavbarVisible }">
+  <div
+    class="z-50 w-full transition-transform duration-300 ease-in-out md:fixed md:top-5"
+    :class="{
+      '-translate-y-full md:-translate-y-[calc(100%+20px)]': !isNavbarVisible,
+    }"
+  >
     <!-- Mobile Header -->
-    <div class="px-4 md:hidden fixed w-full bg-transparent top-0 left-0 justify-between items-center z-50 flex py-4">
-      <HeryakosLogo />
+    <div
+      class="fixed top-0 left-0 z-50 flex w-full items-center justify-between bg-transparent px-4 py-4 md:hidden"
+    >
+      <HeryakosLogo class="w-12" />
       <div @click="handleSidebarOpen" class="size-5 cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
           <path
-            d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+            d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
+          />
         </svg>
       </div>
       <!-- Mobile Sidebar -->
       <div
-        class="fixed max-w-sm bg-black h-screen flex transition-all flex-col origin-right ease-linear duration-700 top-0 right-0 w-0"
-        :class="{ 'w-screen': isSidebarOpen }">
-        <svg @click="handleSidebarOpen" class="size-7 ml-auto mt-4 mr-5 fill-purple-600"
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        class="fixed top-0 right-0 flex h-screen w-0 max-w-sm origin-right flex-col bg-black transition-all duration-700 ease-linear"
+        :class="{ 'w-screen': isSidebarOpen }"
+      >
+        <svg
+          @click="handleSidebarOpen"
+          class="mt-4 mr-5 ml-auto size-7 fill-purple-600"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
           <path
-            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+            d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+          />
         </svg>
-        <ul class="text-white text-center mt-48 space-y-2 text-3xl">
+        <ul class="mt-48 space-y-2 text-center text-3xl text-white">
           <li v-for="(item, index) in navLinks" :key="index">
             <RouterLink :to="{ name: item.name }" @click="handleSidebarOpen">
               {{ item.label }}
@@ -93,20 +101,31 @@ onUnmounted(() => {
     </div>
     <!-- Desktop Heading -->
     <div
-      class="hidden md:flex cursor-pointer justify-between w-[90%] lg:w-[80%] h-full my-auto bg-white mx-auto py-2 rounded-4xl px-3 items-center max-w-[900px]">
-      <HeryakosLogo class="w-[140px] lg:w-[160px]" />
-      <ul class="flex gap-x-4 lg:gap-x-10 text-lg">
-        <RouterLink v-for="(item, index) in navLinks" :key="index" :to="{ name: item.name }" class="py-1 relative"
-          :class="{ 'border-1 px-5 rounded-4xl bg-[#F3F4F7] border-[#E1E4EB]': item.name === route.name }">
+      class="mx-auto my-auto hidden h-full w-[90%] max-w-[900px] cursor-pointer items-center justify-between rounded-4xl bg-white px-3 py-2 md:flex lg:w-[80%]"
+    >
+      <HeryakosLogo class="w-16" />
+      <ul class="flex gap-x-4 text-lg lg:gap-x-10">
+        <RouterLink
+          v-for="(item, index) in navLinks"
+          :key="index"
+          :to="{ name: item.name }"
+          class="relative py-1"
+          :class="{
+            'rounded-4xl border-1 border-[#E1E4EB] bg-[#F3F4F7] px-5':
+              item.name === route.name,
+          }"
+        >
           {{ item.label }}
           <span
-            class="absolute bg-btnColor h-[2px] w-7 bottom-0 left-[33%] opacity-0 transition-all duration-500 ease-linear"
-            :class="{ 'opacity-100': item.name === route.name }"></span>
+            class="bg-btnColor absolute bottom-0 left-[33%] h-[2px] w-7 opacity-0 transition-all duration-500 ease-linear"
+            :class="{ 'opacity-100': item.name === route.name }"
+          ></span>
         </RouterLink>
       </ul>
       <RouterLink :to="{ name: 'ContactUs' }">
         <button
-          class="bg-gradient-to-r cursor-pointer from-[#9B4AF7] to-[#8523F5] text-white px-6 py-2 rounded-full font-medium lg:px-10 lg:py-3">
+          class="cursor-pointer rounded-full bg-gradient-to-r from-[#9B4AF7] to-[#8523F5] px-6 py-2 font-medium text-white lg:px-10 lg:py-3"
+        >
           Contact
         </button>
       </RouterLink>
