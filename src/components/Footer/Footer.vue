52a13b5570a61f15ce5a712bf527f789566f9eff
complatform <kalkidansol1623@gmail.com>
2025-03-17 13:45:03 +0300

    contant change to contact us page and footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -34,6 +34,8 @@ const redirectToTelegram = () => {
         <div
           class="flex size-10 items-center justify-center rounded-full border border-white md:size-12"
         >
+        <a href="https://web.facebook.com/profile.php?id=61559444655752&locale=en_GB">
+          
           <svg
             width="18"
             height="18"
@@ -47,6 +49,7 @@ const redirectToTelegram = () => {
               fill-opacity="0.8"
             />
           </svg>
+        </a>
         </div>
         <div
           class="flex size-10 items-center justify-center rounded-full border border-white md:size-12"
@@ -154,7 +157,8 @@ const redirectToTelegram = () => {
                   d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
                 />
               </svg>
-              info@bizethio.com
+              <p><a href="mailto: yaredyaya16@gmail.com" class="text-blue-500"> yaredyaya16@gmail.com</a></p>
+             
             </li>
             <li class="flex items-center gap-x-2">
               <svg
@@ -165,7 +169,10 @@ const redirectToTelegram = () => {
                 <path
                   d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"
                 /></svg
-              >+251912345678
+                
+              >
+              <p><a href="tel:+251916897173" class="text-blue-500">+251916897173</a></p>
+              
             </li>
           </ul>
 
@@ -173,7 +180,10 @@ const redirectToTelegram = () => {
             <div
               class="flex size-10 items-center justify-center rounded-full border border-white md:size-12"
             >
+            <a href="https://web.facebook.com/profile.php?id=61559444655752&locale=en_GB">
+              
               <svg
+              
                 width="18"
                 height="18"
                 viewBox="0 0 18 18"
@@ -186,6 +196,7 @@ const redirectToTelegram = () => {
                   fill-opacity="0.8"
                 />
               </svg>
+            </a>
             </div>
             <div
               class="flex size-10 items-center justify-center rounded-full border border-white md:size-12"
